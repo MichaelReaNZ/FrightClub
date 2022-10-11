40ae46bb3589682bfd4a9e5b34177a4078137cb4
RichardDasanProject <53543569+RichardDasanProject@users.noreply.github.com>
2022-10-11 15:43:46 +1300

    Update Monster 2
    Movement bug now fixed, has some code relating to illumination but discussion is needed on how to implement this properly.

--- Fright_Club/Assets/Scripts/Monster.cs
+++ Fright_Club/Assets/Scripts/Monster.cs
@@ -4,37 +4,86 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
+    //Textures to be rendered
+    public Sprite darkSprite;
+    public Sprite lightSprite;
+
+    //Positions of player and monster
     private Vector2 PlayerPosition;
     private Vector2 StartingPosition;
-    private float Speed = 0.002f;
+
+    //Speed and Patrol state
+    private float Speed = 0.002f; //ADJUST BASED ON SIZE OF GAME
+    private bool isPatrolling;
+    private bool isIlluminated;
 
     // Start is called before the first frame update
     void Start()
     {
         //Assign starting point for the monster to return
-        StartingPosition = gameObject.transform.position;
+        StartingPosition = this.transform.position;
+        isPatrolling = true;
+        isIlluminated = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Search for the player and move towards them if they are close
+        //Search for the player
         PlayerPosition = GameObject.Find("Player").transform.position;
+        ChangeSprite();
 
-        if( (gameObject.transform.position.x >= PlayerPosition.x - 5 && gameObject.transform.position.y >= PlayerPosition.y - 5 ) ||
-           (gameObject.transform.position.x <= PlayerPosition.x + 5 && gameObject.transform.position.y <= PlayerPosition.y + 5) )
+        //If illuminated, allows movement
+        if (!isIlluminated)
         {
-            gameObject.transform.position = Vector2.MoveTowards(transform.position, PlayerPosition, Speed);
-        }
-        else
-        {
-            gameObject.transform.position = Vector2.MoveTowards(transform.position, StartingPosition, Speed);
+            if ((this.transform.position.x <= PlayerPosition.x + 5 && this.transform.position.y <= PlayerPosition.y + 5) &&
+                (this.transform.position.x >= PlayerPosition.x - 5 && this.transform.position.y >= PlayerPosition.y - 5))
+            {
+                isPatrolling = false;
+                Attack();
+            }
+            else
+            {
+                //Patrol if at starting position
+                if (isPatrolling)
+                {
+                    Patrol();
+                }
+                //Return to starting position
+                else
+                {
+                    this.transform.position = Vector2.MoveTowards(transform.position, StartingPosition, Speed);
+                    if ((Vector2)this.transform.position == StartingPosition)
+                    {
+                        isPatrolling = true;
+                    }
+                }
+            }
         }
     }
 
-    // Move along set routes
+    //Move close to the player to attack them
+    void Attack()
+    {
+        this.transform.position = Vector2.MoveTowards(transform.position, PlayerPosition, Speed);
+    }
+
+    // Move along set routes : TO BE IMPLEMENTED AT A LATER PROTOTYPE
     void Patrol()
     {
 
+    }
+
+    //Changes sprites based on light level
+    void ChangeSprite()
+    {
+        if( isIlluminated )
+        {
+            //this.GetComponent<SpriteRenderer>().sprite = lightSprite;
+        }
+        else
+        {
+            //this.GetComponent<SpriteRenderer>().sprite = darkSprite;
+        }
     }
 }
