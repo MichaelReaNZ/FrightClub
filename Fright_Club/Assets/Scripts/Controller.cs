b93d8c23a1751de193988e3ef8070ace44ae1c9f
Michael Rea <MichaelReaNZ@gmail.com>
2022-10-12 17:27:02 +1300

    360 lantern working but not directional

--- Fright_Club/Assets/Scripts/Controller.cs
+++ Fright_Club/Assets/Scripts/Controller.cs
@@ -19,17 +19,28 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePos = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y));//, viewCamera.transform.position.y));
+       // Vector3 mousePos = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y));//, viewCamera.transform.position.y));
         
         //look in direction of mouse
-        transform.LookAt(mousePos + Vector3.forward * viewCamera.transform.position.z);
+      //  transform.LookAt(mousePos + Vector3.forward * viewCamera.transform.position.z);
         
         //transform.LookAt(mousePos + Vector3.up * transform.position.z);
         
         //velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * moveSpeed;
         
+        
+        
         velocity.x = Input.GetAxisRaw("Horizontal");
         velocity.y = Input.GetAxisRaw("Vertical");
+        
+        velocity = velocity.normalized * moveSpeed;
+        
+        //rotate on z based on the mouse position
+        //transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg - 90);
+        
+       
+           
+       
     }
     
     void FixedUpdate()
--- Fright_Club/Assets/Scripts/FieldOfView.cs
+++ Fright_Club/Assets/Scripts/FieldOfView.cs
@@ -1,6 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
+
 using UnityEngine;
 using System.Collections.Generic;
 
@@ -14,13 +15,26 @@ public class FieldOfView : MonoBehaviour
     public LayerMask obstacleMask;
     
     public List<Transform> visibleTargets = new List<Transform>();
+    
+    public float meshResolution;
+    Mesh viewMesh;
+    
+    public MeshFilter viewMeshFilter;
 
     // Start is called before the first frame update
     void Start()
     {
+        viewMesh = new Mesh();
+        viewMesh.name = "View Mesh";
+        viewMeshFilter.mesh = viewMesh;
+        
         StartCoroutine("FindTargetsWithDelay", .2f);
     }
-    
+
+    private void Update()
+    {
+    }
+
     IEnumerator FindTargetsWithDelay(float delay)
     {
         while (true)
@@ -47,9 +61,66 @@ public class FieldOfView : MonoBehaviour
                 if (!Physics.Raycast(transform.position, dirToTarget, dstToTarget, obstacleMask))
                 {
                     visibleTargets.Add(target);
+                    //change color of target
+                    target.GetComponent<Renderer>().material.color = Color.red;
                     Debug.Log("Target in sight");
                 }
             }
+        }
+    }
+
+    void DrawFieldOfView()
+    {
+        int stepCount = Mathf.RoundToInt(viewAngle * meshResolution);
+        float stepAngleSize = viewAngle / stepCount;
+        List<Vector3> viewPoints = new List<Vector3>();
+
+        for (int i = 0; i <= stepCount; i++)
+        {
+            float angle = transform.eulerAngles.y - viewAngle / 2 + stepAngleSize * i;
+            //rotate angle by 180
+            // angle = angle + 180;
+            // if (angle < 0) angle += 360;
+            //
+            Debug.DrawLine(transform.position, transform.position + DirectionFromAngle(angle, true) * viewRadius, Color.red);
+            ViewCastInfo newViewCast = ViewCast(angle);
+            viewPoints.Add(newViewCast.point);
+        }
+
+        int vertexCount = viewPoints.Count + 1;
+        Vector3[] vertices = new Vector3[vertexCount];
+        int[] triangles = new int[(vertexCount - 2) * 3];
+        
+        vertices[0] = Vector3.zero;
+        for (int i = 0; i < vertexCount - 1; i++)
+        {
+            vertices[i + 1] = transform.InverseTransformPoint(viewPoints[i]);
+            
+            if (i < vertexCount - 2)
+            {
+                triangles[i * 3] = 0;
+                triangles[i * 3 + 1] = i + 1;
+                triangles[i * 3 + 2] = i + 2;
+            }
+        }
+        viewMesh.Clear();
+        viewMesh.vertices = vertices;
+        viewMesh.triangles = triangles;
+        viewMesh.RecalculateNormals();
+    }
+
+    ViewCastInfo ViewCast(float globalAngle)
+    {
+        Vector3 direction = DirectionFromAngle(globalAngle, true);
+        RaycastHit hit;
+        
+        if (Physics.Raycast(transform.position, direction, out hit, viewRadius, obstacleMask))
+        {
+            return new ViewCastInfo(true, hit.point, hit.distance, globalAngle);
+        }
+        else
+        {
+            return new ViewCastInfo(false, transform.position + direction * viewRadius, viewRadius, globalAngle);
         }
     }
     
@@ -66,8 +137,25 @@ public class FieldOfView : MonoBehaviour
     
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+     DrawFieldOfView();   
     }
+    
+    public struct ViewCastInfo
+    {
+        public bool hit;
+        public Vector3 point;
+        public float dst;
+        public float angle;
+
+        public ViewCastInfo(bool _hit, Vector3 _point, float _dst, float _angle)
+        {
+            hit = _hit;
+            point = _point;
+            dst = _dst;
+            angle = _angle;
+        }
+    }
+
 }
